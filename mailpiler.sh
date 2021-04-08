9e52b4b966791c6a8d38a1736e8eabfd4e105a6a
Thorsten Spille <thorsten@spille-edv.de>
2021-04-08 21:15:26 +0200

    Configure locales and timezone

--- mailpiler.sh
+++ mailpiler.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Authors:
+# (C) 2021 Idea an concept by Christian Zengel <christian@sysops.de>
+# (C) 2021 Script design and prototype by Markus Helmke <helmke@cloudistboese.de>
+# (C) 2021 Script rework by Thorsten Spille <thorsten@spille-edv.de>
+
 source ./zamba.conf
+
+# Set Timezone
+ln -sf /usr/share/zoneinfo/$LXC_TIMEZONE /etc/localtime
+
+# configure system language
+dpkg-reconfigure locales
 
 HOSTNAME=$(hostname -f)
 
