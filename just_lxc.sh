93d4d1fe04fb1d251ee749e7421fe053cba6a335
Thorsten Spille <thorsten@spille-edv.de>
2021-04-08 21:14:14 +0200

    Configure timzone and locales

--- just_lxc.sh
+++ just_lxc.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "This is just a dummy script"
+# Authors:
+# (C) 2021 Idea an concept by Christian Zengel <christian@sysops.de>
+# (C) 2021 Script design and prototype by Markus Helmke <helmke@cloudistboese.de>
+# (C) 2021 Script rework by Thorsten Spille <thorsten@spille-edv.de>
+
+# Set Timezone
+ln -sf /usr/share/zoneinfo/$LXC_TIMEZONE /etc/localtime
+
+dpkg-reconfigure locales
