3d46ec1f7a951f648e5b6db8f2ee1119a8bdf1ef
Thorsten Spille <thorsten@spille-edv.de>
2021-04-08 21:16:22 +0200

    Configure locales and timezone

--- zmb_member.sh
+++ zmb_member.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Authors:
+# (C) 2021 Idea an concept by Christian Zengel <christian@sysops.de>
+# (C) 2021 Script design and prototype by Markus Helmke <helmke@cloudistboese.de>
+# (C) 2021 Script rework by Thorsten Spille <thorsten@spille-edv.de>
+
 source ./zamba.conf
+
+# Set Timezone
+ln -sf /usr/share/zoneinfo/$LXC_TIMEZONE /etc/localtime
+
+# configure system language
+dpkg-reconfigure locales
 
 apt update && apt full-upgrade -y
 echo -ne '\n' | apt install -y $LXC_TOOLSET acl samba winbind libpam-winbind libnss-winbind krb5-user krb5-config samba-dsdb-modules samba-vfs-modules 
