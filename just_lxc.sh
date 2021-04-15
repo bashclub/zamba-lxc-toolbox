7ce2102dfe288958140b4431fdffe4f2920813f3
Thorsten Spille <thorsten@spille-edv.de>
2021-04-15 17:40:02 +0000

    Activating vim syntax highlighting

--- just_lxc.sh
+++ just_lxc.sh
@@ -15,3 +15,4 @@ ln -sf /usr/share/zoneinfo/$LXC_TIMEZONE /etc/localtime
 apt update
 DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt -y -qq dist-upgrade
 DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt install -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" $LXC_TOOLSET
+sed -i "s|\"syntax on|syntax on|g" /etc/vim/vimrc
