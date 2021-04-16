951348521d63f792913e71a2e51aba8d1082d5b2
DerFossiBaer <56678897+DerFossiBaer@users.noreply.github.com>
2021-04-16 17:00:31 +0200

    Update just_lxc.sh

--- just_lxc.sh
+++ just_lxc.sh
@@ -2,7 +2,7 @@
 
 # Authors:
 # (C) 2021 Idea an concept by Christian Zengel <christian@sysops.de>
-# (C) 2021 Script design and prototype by Markus Helmke <helmke@cloudistboese.de>
+# (C) 2021 Script design and prototype by Markus Helmke <m.helmke@nettwarker.de>
 # (C) 2021 Script rework and documentation by Thorsten Spille <thorsten@spille-edv.de>
 
 dpkg-reconfigure locales
