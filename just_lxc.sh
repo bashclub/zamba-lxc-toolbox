ea60e768dedfefcae5a8db5c4ebeadf8bc91fa69
Thorsten Spille <thorstenspille@users.noreply.github.com>
2021-04-20 21:48:21 +0200

    Changed setting locales #20

--- just_lxc.sh
+++ just_lxc.sh
@@ -5,9 +5,14 @@
 # (C) 2021 Script design and prototype by Markus Helmke <m.helmke@nettwarker.de>
 # (C) 2021 Script rework and documentation by Thorsten Spille <thorsten@spille-edv.de>
 
-dpkg-reconfigure locales
-
 source /root/zamba.conf
+
+sed -i "s|# $LXC_LOCALE|$LXC_LOCALE|" /etc/locale.gen
+cat << EOF > /etc/default/locale
+LANG="$LXC_LOCALE"
+LANGUAGE=$LXC_LOCALE
+EOF
+locale-gen $LXC_LOCALE
 
 # Set Timezone
 ln -sf /usr/share/zoneinfo/$LXC_TIMEZONE /etc/localtime
