bfcb4d6c840b577314f1b5e6b2e3be727e34605f
Thorsten Spille <thorstenspille@users.noreply.github.com>
2021-04-20 21:51:13 +0200

    Changed setting locale #20

--- zmb-ad.sh
+++ zmb-ad.sh
@@ -5,10 +5,14 @@
 # (C) 2021 Script design and prototype by Markus Helmke <m.helmke@nettwarker.de>
 # (C) 2021 Script rework and documentation by Thorsten Spille <thorsten@spille-edv.de>
 
-# configure system language
-dpkg-reconfigure locales
-
 source /root/zamba.conf
+
+sed -i "s|# $LXC_LOCALE|$LXC_LOCALE|" /etc/locale.gen
+cat << EOF > /etc/default/locale
+LANG="$LXC_LOCALE"
+LANGUAGE=$LXC_LOCALE
+EOF
+locale-gen $LXC_LOCALE
 
 if [[ $ZMB_DNS_BACKEND == "BIND9_DLZ" ]]; then
   BINDNINE=bind9
