f223a0741713b95d546b03af61f10403c5634212
pLAn3tT30 <106966424+pLAn3tT30@users.noreply.github.com>
2022-06-06 13:51:54 +0200

    open3A random pwd max. 20

--- src/functions.sh
+++ src/functions.sh
@@ -6,3 +6,8 @@ random_password() {
     set +o pipefail
     C_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c32
 }
+
+random_password_open3a() {
+    set +o pipefail
+    C_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20
+}
