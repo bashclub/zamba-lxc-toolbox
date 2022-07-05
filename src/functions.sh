261770dec53ece404a247e17e18677a2a20d8415
thorstenspille <thorsten@spille-edv.de>
2022-07-05 20:58:03 +0200

    Change password generation to dynamic length

--- src/functions.sh
+++ src/functions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 # This script has basic functions like a random password generator
+LXC_RANDOMPWD=32
 
 random_password() {
     set +o pipefail
-    C_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c32
+    C_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c${LXC_RANDOMPWD}
 }
