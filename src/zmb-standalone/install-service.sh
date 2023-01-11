666e2b320cdb989b6bdae2f34cc5589c6fca4b1f
thorstenspille <thorsten@spille-edv.de>
2023-01-11 19:58:24 +0100

    Fix zmb-standalone

--- src/zmb-standalone/install-service.sh
+++ src/zmb-standalone/install-service.sh
@@ -17,13 +17,13 @@ echo "deb https://pkg.ltec.ch/public/ $(lsb_release -cs) main" > /etc/apt/source
 echo "deb http://ftp.de.debian.org/debian $(lsb_release -cs)-backports main contrib" > /etc/apt/sources.list.d/$(lsb_release -cs)-backports.list
 
 cat << EOF > /etc/apt/preferences.d/samba
-Package: samba
+Package: samba*
 Pin: release a=$(lsb_release -cs)-backports
 Pin-Priority: 900
 EOF
 
 cat << EOF > /etc/apt/preferences.d/winbind
-Package: winbind
+Package: winbind*
 Pin: release a=$(lsb_release -cs)-backports
 Pin-Priority: 900
 EOF
@@ -59,6 +59,7 @@ cat << EOF | sudo tee -i /etc/samba/import.template
     max log size = 1000
     logging = file
     panic action = /usr/share/samba/panic-action %d
+    log level = 3
     server role = standalone server
     obey pam restrictions = yes
     unix password sync = yes
@@ -66,34 +67,30 @@ cat << EOF | sudo tee -i /etc/samba/import.template
     passwd chat = *Enter\snew\s*\password:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
     pam password change = yes
     map to guest = bad user
+    vfs objects = shadow_copy2 acl_xattr catia fruit streams_xattr
     map acl inherit = yes
     acl_xattr:ignore system acls = yes
-    vfs objects = shadow_copy2 acl_xattr catia fruit streams_xattr full_audit
     shadow: snapdir = .zfs/snapshot
     shadow: sort = desc
     shadow: format = -%Y-%m-%d-%H%M
     shadow: snapprefix = ^zfs-auto-snap_\(frequent\)\{0,1\}\(hourly\)\{0,1\}\(daily\)\{0,1\}\(weekly\)\{0,1\}\(monthly\)\{0,1\}
     shadow: delimiter = -20
     fruit:encoding = native
-	fruit:metadata = stream
-	fruit:zero_file_id = yes
-	fruit:nfs_aces = no
-    full_audit:priority = notice
-    full_audit:facility = local5
-    full_audit:success = connect disconnect mkdir rmdir read write rename
-    full_audit:failure = connect
-    full_audit:prefix = %u|%I|%S
+    fruit:metadata = stream
+    fruit:zero_file_id = yes
+    fruit:nfs_aces = no
 EOF
 
 net conf import /etc/samba/import.template
 
-net conf addshare $ZMB_SHARE /$LXC_SHAREFS_MOUNTPOINT/$ZMB_SHARE
-net conf setparm $ZMB_SHARE readonly no
-net conf setparm $ZMB_SHARE createmask 0660
-net conf setparm $ZMB_SHARE directorymask 0770
-
 mkdir -p /$LXC_SHAREFS_MOUNTPOINT/$ZMB_SHARE
 chmod -R 770 /$LXC_SHAREFS_MOUNTPOINT/$ZMB_SHARE
 chown -R $USER:root /$LXC_SHAREFS_MOUNTPOINT/$ZMB_SHARE
+
+net conf addshare $ZMB_SHARE /$LXC_SHAREFS_MOUNTPOINT/$ZMB_SHARE
+net conf setparm $ZMB_SHARE readonly no
+net conf setparm $ZMB_SHARE browseable yes
+net conf setparm $ZMB_SHARE createmask 0660
+net conf setparm $ZMB_SHARE directorymask 0770
 
 systemctl restart smbd nmbd wsdd
