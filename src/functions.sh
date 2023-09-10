5802c2c04337f0f04d629f25a37547df5bec11a6
thorstenspille <thorsten@spille-edv.de>
2023-09-10 11:17:10 +0200

    Add dhparam generation function

--- src/functions.sh
+++ src/functions.sh
@@ -7,3 +7,14 @@ random_password() {
     set +o pipefail
     LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom 2>/dev/null | head -c${LXC_RANDOMPWD}
 }
+
+generate_dhparam() {
+    openssl dhparam -out /etc/nginx/dhparam.pem 2048
+    cat << EOF > /etc/cron.weekly/generate-dhparams
+#!/bin/bash
+openssl dhparam -out /etc/nginx/dhparam.gen 4096 > /dev/null 2>&1
+mv /etc/nginx/dhparam.gen /etc/nginx/dhparam.pem
+systemctl restart nginx
+EOF
+    chmod +x /etc/cron.weekly/generate-dhparams
+}
